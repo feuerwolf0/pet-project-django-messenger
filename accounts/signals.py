ea9eb4b0770bb03ab11c92f7fa504b45b1cf21bb
Poletaev Kirill <poletaev777@gmail.com>
2024-01-24 13:59:38 +0300

    Добавил сигнал обновления user после редактирования аккаунта

---
+++ accounts/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+
+from accounts.models import Account
+
+
+def update_user_signal(sender, instance, created, **kwargs):
+    if not created:
+        user = instance.user
+        user.email = instance.email
+        user.first_name = instance.first_name
+        user.last_name = instance.last_name
+        user.save()
+
+
+post_save.connect(update_user_signal, sender=Account)
